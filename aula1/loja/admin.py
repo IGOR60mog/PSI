a9d74e6d0187e46e500da88cc4488d69ebdc2150
IGOR BATISTA MARQUES DA SILVA <20231011110004@ifrn.local>
2025-04-28 14:30:31 -0300

    Produtos

--- aula1/loja/admin.py
+++ aula1/loja/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin #isso já vai estar existindo no
 
 # Register your models here.
+from .models import * #imporata nossos models
+admin.site.register(Fabricante) #adiciona a interface do adm
+admin.site.register(Categoria)
+admin.site.register(Produto)
