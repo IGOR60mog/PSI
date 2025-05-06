1b6594507c8d1513a9cc0b7581153ef89b5cfc55
IGOR BATISTA MARQUES DA SILVA <20231011110004@ifrn.local>
2025-05-06 15:33:06 -0300

    Promocao censura

--- aula1/loja/models/Produto.py
+++ aula1/loja/models/Produto.py
@@ -9,11 +9,14 @@ class Produto(models.Model):
     related_name='categoria', on_delete=models.SET_NULL)
     fabricante = models.ForeignKey(Fabricante, null=True,
     related_name='fabricante', on_delete=models.SET_NULL)
+    msgPromocao = models.CharField(null=True, max_length=100,
+blank=True)
 
     #loja/models/Produto.py parte 2:
     criado_em = models.DateTimeField(auto_now_add=True)
     alterado_em = models.DateTimeField(auto_now=True)
     image = models.ImageField(null=True, blank=True)
+    
     
     def __str__(self):
         return '{}'.format(self.Produto)
