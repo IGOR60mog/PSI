cc818bb5c29e87f9f22c8e2f26a7047c0c0aa17e
IGOR BATISTA MARQUES DA SILVA <20231011110004@ifrn.local>
2025-06-16 14:30:08 -0300

    Templates pg45 (conferir html)

--- aula1/loja/views/ProdutoView.py
+++ aula1/loja/views/ProdutoView.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render # Retire from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
-
 def list_produto_view(request, id=None):
-
     produto = request.GET.get("produto")
     destaque = request.GET.get("destaque")
     promocao = request.GET.get("promocao")
@@ -12,12 +10,8 @@ def list_produto_view(request, id=None):
     fabricante = request.GET.get("fabricante")
     dias = request.GET.get("dias")
     produtos = Produto.objects.all()
-    if dias is not None:
-        now = timezone.now()
-        now = now - timedelta(days = int(dias))
-        produtos = produtos.filter(criado_em__gte=now)
     if produto is not None:
-        produtos = produtos.filter(Produto=produto)
+        produtos = produtos.filter(Produto__contains=produto)
     if promocao is not None:
         produtos = produtos.filter(promocao=promocao)
     if destaque is not None:
@@ -26,10 +20,15 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
         produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
     if id is not None:
         produtos = produtos.filter(id=id)
-    print(produtos)
-    if id is None:
-        return HttpResponse('<h1>Nenhum id foi informado</h1>')
-
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+        print(produtos)
+    # Adicione para definir o contexto e carregar o template
+    context = {
+    'produtos': produtos
+    }
+    return render(request, template_name='produto/produto.html', context=context, status=200)
